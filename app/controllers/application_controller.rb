d1c46e99e16374a4b2f24a64677bdac11b0482a9
shirokane <jcanete@aoiths.org>
2015-02-08 13:43:46 -0500

    forms and css styled

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,22 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
+  get '/' do
+    
+    # tweet1 = Tweet.new("Danny","Hello")
+    # tweet2 = Tweet.new("bob","Hello")
+    # tweet3 = Tweet.new("fkljsnfk","Hello")
+
+    @tweets = Tweet.all_messages
+
+    erb :tweets
+  end
+
+  post '/tweets' do
+    # puts params
+    # binding.pry
+    new_tweet = Tweet.new(params[:user],params[:message])
+    redirect('/')
+  end
+
 end
